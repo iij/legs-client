c7da104025940ea6fa6fc757decccfaf58954fb6
r-fujimoto <regunorf@gmail.com>
2019-07-04 20:31:29 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,18 +16,3 @@ gox -ldflags="-s -w -X main.version=$VERSION -X main.hash=$HASH -X \"main.buildd
 	-os="linux netbsd darwin" \
 	-output="./dist/legsc_{{.OS}}_{{.Arch}}" \
 	./app/...
-
-export GITHUB_API=http://gh.iiji.jp/api/v3
-
-cd dist
-files="*"
-for file in ${files}; do
-  tar -zcvf ${file}.tar.gz ${file}
-
-  github-release upload \
-    --user legs-v2 \
-    --repo legs-client \
-    --tag ${VERSION} \
-    --name ${file}.tar.gz \
-    --file ${file}.tar.gz
-done
